d31689965d82d49c8d4d2acb4360cd8d4593e1df
fremix <microsys.net@gmail.com>
2022-02-15 00:24:51 -0600

    arrays

--- js/14.js
+++ js/14.js
@@ -2,5 +2,27 @@
  * Arreglos o arrays
  */
 
+const numeros = [10,20,30,40,50];
+console.table(numeros);
+
+const meses = ['Enero', 'Febrero', 'Marzo','Abril','Mayo'];
+console.table(meses);
+
+/**
+ * const arreglo = ["Hola", 10, true, "si", null, {nombre : "Freddy", trabajo: "Desarrollador"}, [1,2,3]];
+console.table(arreglo[6]);
+ */
+
+/**
+ * Acceder a los valores de un arreglo
+ */
+console.log(numeros[4]);
+console.log(numeros[2]);
+console.log(numeros[200]);
+
+/**
+ * Conocer la extension de un arreglo
+ */
+console.log(numeros.length);
 
 
