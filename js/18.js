2cf5983529e8a50f675e11be9e5ba5d536b9ed43
fremix <microsys.net@gmail.com>
2022-03-10 11:39:29 -0600

    Funciones con parametros y argumentos

---
+++ js/18.js
@@ -0,0 +1,23 @@
+
+function sumar(numero1, numero2) { // numero1 y numero2 son parametros
+    console.table(numero1 + numero2);
+};
+sumar( 10, 10); // Argumentos o valores reales
+sumar( 3, 3);
+sumar( 3, 6);
+sumar( 1, 2);
+
+/***********/
+
+const sumar2 = function(n1, n2) { Parametros
+    console.log( n1 + n2);
+};
+sumar2(5, 10); // Argumentos
+
+
+
+function fremi (nombre, appat, apmat) {
+    console.log('nombre',+ 'apmat', +'apmat');
+};
+
+fremi('Freddy Guadalupe', +'Morales', +'Izquierdo');
