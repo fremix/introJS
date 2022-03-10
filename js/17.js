2d6b254963c81ab21fab2985e00aa660f0c9ae06
fremix <microsys.net@gmail.com>
2022-03-10 01:41:35 -0600

    Diferencia entre metodo y funcion

--- js/17.js
+++ js/17.js
@@ -1,2 +1,10 @@
+// Diferencia entre metodo y funcion 
+
 const numero1 = 20;
 const numero2 = '20';
+
+console.log( parseInt(numero2) );  // parseInt() es una Funcion 
+console.log( numero1.toString() ); // toString() es un metodo
+/* Los que tiene un punto seguido del nombre de la funcion son metodos
+*  LOs que tiene el nombre de la funcion seguido del parentesos son Funciones
+*/
