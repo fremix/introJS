0645f4f34d0b003990672e61bb5affa09da700d2
fremix <microsys.net@gmail.com>
2022-04-08 23:08:59 -0500

    Operador estricto

--- js/018-Comparacion y Operador estricto.js
+++ js/018-Comparacion y Operador estricto.js
@@ -4,10 +4,25 @@ const numero1 = 20;
 
 const numero2 = '20';
 
-if( numero1 === numero2)   {
+/**
+ *  == (   Comparacion pero no es estricto   );
+ *  === (   Comparacion estricta - Revisa el valor y el tipo de dato  )
+ */
+
+if( numero1 === Number(numero2))   { /** se usa Number para modificar un tipo de dato  */
     console.log('Si son iguales');
 } else {
-    console.log('Son diferente');
+    console.log('Son diferentes');
 }
 
 
+
+const autenticado = true;
+if(autenticado) {
+    console.log('Si esta autenticado');
+};
+
+const logIn = false;
+if(logIn === false) {
+    console.log('Desconectado');
+}
