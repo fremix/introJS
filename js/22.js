7753dd607ac3792631b233826f0ecdd3814331a5
fremix <microsys.net@gmail.com>
2022-05-08 11:23:04 -0500

    Estructuras de control

---
+++ js/22.js
@@ -0,0 +1,47 @@
+/** Estructuras de Control **/
+/**
+const puntaje = 100;
+
+if (puntaje !== 1000) {
+    console.log('No es igual');
+    
+} else {
+    console.log('Si el puntaje es 1000');
+}
+
+const efectivo = 1000;
+const carrito = 800;
+
+if (efectivo > carrito) {
+    console.log('El usuario si puede pagar');
+} else {
+    console.log('Fondos insuficientes');
+}
+*/
+
+const rol = 'VISITANTE';
+
+if (rol === 'ADMINISTRADOR') {
+    console.log('Acceso a todo el sistema');
+
+}  else if (rol === 'EDITOR') {
+    console.log('Eres editor, puedes entrar pero no puedes hacer mucho');
+
+} else if (rol === 'PUBLICADOR') {
+    console.log('Solo puedes publicar');
+
+} else {
+    console.log('No tienes acceso');
+}
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ js/23.js
@@ -0,0 +1,26 @@
+/**  Switch **/
+
+const metodoPago = 'tarjeta';
+
+switch(metodoPago) {
+    case 'tarjeta':
+        console.log('Pagaste con tarjeta');
+        break;
+    default:
+        console.log('Aún no has pagado');
+        break;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
